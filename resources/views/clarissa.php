426b4c539cafd094b540e425f68128f194316b7d
clarissa05 <deveraclarissa5@gmail.com>
2020-12-16 16:24:48 -0800

    My first commit

---
+++ resources/views/clarissa.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>	
+		<title>Clarissa</title>
+	</head>
+	<style>
+		body {
+	background-color: grey;
+	background-size: cover;
+}
+th {
+	background-color:teal;
+	color:black;
+	padding:25px;
+	border-radius: 10px 10px 0 0;
+}
+.foot {
+	border-radius: 0 0 10px 10px;
+	padding:15px;
+	background-color:#3e3e3e;
+	color:#d7d7d7;
+}
+.body {
+	background-color:#15151E;
+	color:#d7d7d7;
+}
+ul li {
+	list-style:none;
+	padding:3px;
+	width:184px;
+}
+li a{
+	text-decoration: none;
+	color:black;
+	background-color:white;
+	padding:20px;
+	display:block;
+}
+li a:hover{
+	text-decoration: none;
+	color:teal;
+	background-color:#3E3E32;
+	padding:20px;
+	display:block;
+}
+	</style>
+	<body style="background-image: url(https://source.unsplash.com/650x550)">
+
+		<table align="center">
+			<tr>
+				<th colspan="2"><h1>My Personal Portfolio!</h1></th>
+			</tr>
+			<tr class="body">
+				<td>
+					<ul>
+						<li><a href="/home">Home</a></li>
+						<li><a href="/about">About</a></li>
+						<li><a href="/contact">Contact</a></li>
+					</ul>
+				</td>
+				<td>
+					<h2>My Portfolio</h2>
+					<h3>I'm having fun while doing this</h3>
+					<p>This is my second time to make <b>Portfolio</b> in my new subject Advance Web Development. This is going to be<br>
+					great. I am so excited I can hardly <b>contain myself</b>. Don't you just love<br>
+					Portfolio? I find them very useful in finding information of someone.</p>
+					<h4>Web Pages Are Exciting Too</h4>
+					<p>Yes, that's right - web pages can be a lot of fun. Learning how to create<br>
+					web pages is easy and <b>entertaining.</b>
+				</td>
+			</tr>
+			<tr>
+				<td class="foot" colspan="2" align="center">&copy Clarissa De Vera 2020</td>
+			</tr>
+		</table>
+	</body>
+</html>
